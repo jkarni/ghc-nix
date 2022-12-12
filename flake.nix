e84e74caeea20e0316b027d87c6fbcdce123ca2f
Julian K. Arni <jkarni@gmail.com>
2022-12-12 14:46:51 +0100

    withCabal option

--- flake.nix
+++ flake.nix
@@ -21,6 +21,29 @@
         rec {
           overlays.default = overlay;
           packages.default = pkgs.haskell.packages."${compiler}".ghc-nix;
+          packages.withCabal = pkgs.stdenv.mkDerivation {
+            name = "with-cabal";
+            buildInputs = with pkgs; [ ghc bash nix which coreutils jq gnused rsync ];
+            src = ./ghc-nix;
+            requiredSystemFeatures = [ "recursive-nix" ];
+            buildPhase = ''
+              # see https://github.com/haskell/cabal/issues/5783#issuecomment-464136859
+              mkdir .cabal
+              touch .cabal/config
+              ${pkgs.hpack}/bin/hpack
+              export PATH=$NIX_GHCPKG:$PATH
+              eval $(grep export ${pkgs.ghc}/bin/ghc)
+              nix --version
+              ${packages.default}/bin/ghc-nix --numeric-version
+              ${pkgs.cabal-install}/bin/cabal build --enable-tests -w "${packages.default}/bin/ghc-nix"
+            '';
+            checkPhase = ''
+              ${pkgs.cabal-install}/bin/cabal test -w "${packages.default}/bin/ghc-nix"
+            '';
+            installPhase = ''
+              ${pkgs.cabal-install}/bin/cabal install --output-dir=$out
+            '';
+          };
 
           packages.self-test = pkgs.haskell.packages."${compiler}".callPackageIncrementally ./ghc-nix {};
           # This currently fails to compile ghc-nix
@@ -44,6 +67,6 @@
 
           devShell = devShells.default;
 
-     }
+      }
    );
 }
